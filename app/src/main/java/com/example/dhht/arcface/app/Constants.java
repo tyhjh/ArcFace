bc6e8c97e5a05ad337c1198b43626cc54da08887
HanPei <tyhj5@qq.com>
2019-04-11 12:56:46 +0800

    设置了默认appkey

--- app/src/main/java/com/example/dhht/arcface/app/Constants.java
+++ app/src/main/java/com/example/dhht/arcface/app/Constants.java
@@ -2,15 +2,16 @@ package com.example.dhht.arcface.app;
 
 public class Constants {
 
-    public static String FREESDKAPPID = "";
-    public static String FDSDKKEY = "";
-    public static String FTSDKKEY = "";
-    public static String FRSDKKEY = "";
+    public static String FREESDKAPPID = "77eipio99QPfj3p9n9vd7wSvgup8vBv7YExpLPB9Nt53";
+    public static String FDSDKKEY = "6SrG1Dev5vo9RsYeBzR8j8D9NgJ6HraFMWTQ5dJPcLuQ";
+    public static String FTSDKKEY = "6SrG1Dev5vo9RsYeBzR8j8D2DH2vc73NQ1P1bQxSSqWm";
+    public static String FRSDKKEY = "6SrG1Dev5vo9RsYeBzR8j8De2HLn22nzqpLfiHA9w765";
 
-    public static String IDCARDAPPID = "";
-    public static String IDCARDSDKKEY = "";
 
-    public static String LIVENESSAPPID = "";
-    public static String LIVENESSSDKKEY = "";
+    public static String IDCARDAPPID = "77eipio99QPfj3p9n9vd7wSvgup8vBv7YExpLPB9Nt53";
+    public static String IDCARDSDKKEY = "6SrG1Dev5vo9RsYeBzR8j8DGY5ZDmkmGmP4jTXjvZ7o9";
+
+    public static String LIVENESSAPPID = "77eipio99QPfj3p9n9vd7wSvgup8vBv7YExpLPB9Nt53";
+    public static String LIVENESSSDKKEY = "6SrG1Dev5vo9RsYeBzR8j8Z2UFmkjKeX91ZGNPoGoTfs";
 
 }
